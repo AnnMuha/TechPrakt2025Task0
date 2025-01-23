a3319f1f3701e108659286b1bca8a63ecdb1a671
Ann Muha <ann.muha1209@gmail.com>
2025-01-23 19:22:13 +0200

    added to folder .gitignore and SumCalculator

---
+++ SumCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace SumCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введіть перше число:");
+            if (!double.TryParse(Console.ReadLine(), out double number1))
+            {
+                Console.WriteLine("Помилка: потрібно ввести число!");
+                return;
+            }
+
+            Console.WriteLine("Введіть друге число:");
+            if (!double.TryParse(Console.ReadLine(), out double number2))
+            {
+                Console.WriteLine("Помилка: потрібно ввести число!");
+                return;
+            }
+
+            double sum = number1 + number2;
+
+            Console.WriteLine($"Сума чисел {number1} та {number2} дорівнює {sum}");
+        }
+    }
+}
